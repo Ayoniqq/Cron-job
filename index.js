a720f8061f93a551d44076ad9fc63c2d326b7b48
Ayoniqq <ayorindeadeshina@gmail.com>
2023-03-05 13:36:52 +0100

    cron-job

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const PORT = 8080;
 const cron = require("node-cron");
 
+cron.schedule("* * * * *", () => {
+  console.log("PRINT SOMETHING");
+});
+
 app.listen(PORT, () => {
   console.log(`LISTENING ON PORT: ${PORT}`);
 });
