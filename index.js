27c0958e8a63d662e78f007146fc99cb43f10e59
Ayoniqq <ayorindeadeshina@gmail.com>
2023-03-06 14:03:09 +0100

    commersa

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express();
 const PORT = 8080;
 const cron = require("node-cron");
 
-cron.schedule("* * * * *", () => {
-  console.log("PRINT SOMETHING");
-});
+// cron.schedule("* * * * *", () => {
+//   console.log("PRINT SOMETHING");
+// });
 
 app.listen(PORT, () => {
   console.log(`LISTENING ON PORT: ${PORT}`);
 
-  cron.schedule("10 * * * * *", () => {
+  cron.schedule("5 * * * * *", () => {
     console.log("TASK IS RUNNING");
   });
 });
